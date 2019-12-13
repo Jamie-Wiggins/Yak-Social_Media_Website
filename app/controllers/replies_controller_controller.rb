f6be7f4e74a7f305451f32a6ee1912e30bdd7c2f
Jamie Wiggins <jamiewiggins99@gmail.com>
2019-12-13 22:53:49 +0000

    Created pages, groups, posts, registrations, relationships, replies controllers

---
+++ app/controllers/replies_controller_controller.rb
@@ -0,0 +1,2 @@
+class RepliesControllerController < ApplicationController
+end
---
+++ test/controllers/replies_controller_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class RepliesControllerControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
