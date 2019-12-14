c3492c77ac878e48fe14d8ae5795cdd18240a1d4
Jamie Wiggins <jamiewiggins99@gmail.com>
2019-12-14 00:06:54 +0000

    devise views created and formatted

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,27 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users, controllers: { registrations: 'registrations' }
+ 
+  resources :users do
+    member do
+      get :following, :followers
+    end
+  end
+  
+  resources :relationships
+  # , only: [:create, :destroy]
+  resources :posts do
+    resources :replies
+  end
+
+  resources :groups
+
+  # Define Root URL
+  root 'pages#home'
+  
+  # Define Routes for Pages
+  get '/user/:id' => 'pages#profile'
+  get '/group/:id' => 'pages#group'
+  get 'search', to: "posts#search"
+  get 'contact', to: "pages#contact"
+  post 'request_contact', to: "pages#request_contact"
 end
